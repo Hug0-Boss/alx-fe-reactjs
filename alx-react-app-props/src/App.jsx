24202f685fd0ebbe6e3d643c338cb47e71a0f8df
Ugochukwu Ebifa <ebifaugo@gmail.com>
2025-01-18 03:04:01 +0100

    my-company

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -9,6 +9,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
