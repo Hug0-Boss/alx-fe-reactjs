a9d78cd0193b24a0e1a794bf8ef8f0e23642e402
Ugochukwu Ebifa <ebifaugo@gmail.com>
2025-01-18 12:56:17 +0100

    recipe-sharing-app

---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,13 @@
+import { useRecipeStore } from './recipeStore';
+import { useNavigate } from 'react-router-dom';
+
+const DeleteRecipeButton = ({ recipeId }) => {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate;
+
+  return (
+    <button onClick={() => deleteRecipe(recipeId)}>Delete Recipe</button>
+  );
+};
+
+export default DeleteRecipeButton;
